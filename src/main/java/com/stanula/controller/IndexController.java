89dba8ea99313636956c003db66207799662d0bd
rafalstanula <catamenia91@gmail.com>
2017-11-24 20:48:53 +0100

    Added index page mapping.

--- src/main/java/com/stanula/controller/IndexController.java
+++ src/main/java/com/stanula/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.stanula.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String mainPage(){
+        return "index";
+    }
 
 }
