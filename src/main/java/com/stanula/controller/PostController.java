e42601399126a4c15da0e1ad8fa955324a520da0
rafalstanula <catamenia91@gmail.com>
2017-11-27 23:08:03 +0100

    Corrected classes: Posts, IndexController, PostController.

--- src/main/java/com/stanula/controller/PostController.java
+++ src/main/java/com/stanula/controller/PostController.java
@@ -1,23 +1,15 @@
 package com.stanula.controller;
 
-import com.stanula.domain.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public String listPosts(Model model){
-        List<Post> postsList = new ArrayList<Post>();
-        postsList.add(Post.createInstance("Post1"));
-        postsList.add(Post.createInstance("Post2"));
-        model.addAttribute("posts", postsList);
+    @GetMapping ("/posts")
+    public String getPosts(Model model){
+        model.addAttribute("posts");
         return "posts";
     }
 
--- src/main/java/com/stanula/domain/Post.java
+++ src/main/java/com/stanula/domain/Post.java
@@ -2,13 +2,23 @@ package com.stanula.domain;
 
 public final class Post {
 
-    private final String postName;
+    private final String author;
+    private final String content;
 
-    private Post(String postName) {
-        this.postName = postName;
+    private Post(String author, String content) {
+        this.author = author;
+        this.content = content;
     }
 
-    public static Post createInstance(String postName){
-        return new Post(postName);
+    public static Post createPost (final String author, final String content){
+        return new Post(author, content);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
