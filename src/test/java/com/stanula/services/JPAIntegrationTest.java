cff7364ccc159e88a2bd3a7ee29cd88d5f576875
Rafał <catamenia91@gmail.com>
2018-03-01 15:09:16 +0100

    1. Implemented first test in JPAIntegrationTest. 2. Added default constructor and getId method in domain class.

--- src/test/java/com/stanula/services/JPAIntegrationTest.java
+++ src/test/java/com/stanula/services/JPAIntegrationTest.java
@@ -1,12 +1,16 @@
 package com.stanula.services;
 
 import com.stanula.BlogApplication;
+import com.stanula.domain.Post;
 import com.stanula.repositories.PostsRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = BlogApplication.class)
@@ -14,4 +18,14 @@ public class JPAIntegrationTest {
 
     @Autowired
     private PostsRepository postsRepository;
+
+    @Test
+    public void givenOnePost_whenSaveAndRetrievePost_thenRetrievedPostIsNotNull() {
+        Post redPost = Post.createPost("John", "My red post");
+
+        Post savedPost = postsRepository.save(redPost);
+        Post foundPost = postsRepository.findOne(savedPost.getId());
+
+        assertNotNull(foundPost);
+    }
 }
