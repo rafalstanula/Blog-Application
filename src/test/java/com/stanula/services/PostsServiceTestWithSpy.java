4fe6f0fbe4df66568e0ccd83ad281c521f81b9bc
Rafał <catamenia91@gmail.com>
2018-03-02 12:30:06 +0100

    Added 2 tests to PostsServiceTestWithSpy class.

--- src/test/java/com/stanula/services/PostsServiceTestWithSpy.java
+++ src/test/java/com/stanula/services/PostsServiceTestWithSpy.java
@@ -1,11 +1,15 @@
 package com.stanula.services;
 
 import com.stanula.repositories.PostsRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,5 +19,19 @@ public class PostsServiceTestWithSpy {
     private PostsService postsService;
 
     @Spy
-    private PostsRepository repositoryMock;
+    private PostsRepository repositorySpy;
+
+    @Test
+    public void givenPostsRepositorySpy_whenGetPostsMethodIsCall_thenFindAllMethodIsInvocatedOnce() {
+        postsService.getPosts();
+
+        verify(repositorySpy, times(1)).findAll();
+    }
+
+    @Test
+    public void givenPostsRepositorySpy_whenGetPostsMethodWithParameterIsCall_thenFindAllByAuthorMethodIsInvocatedOnce() {
+        postsService.getPosts("John");
+
+        verify(repositorySpy, times(1)).findAllByAuthor("John");
+    }
 }
